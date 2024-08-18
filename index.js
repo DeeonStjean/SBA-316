d8635667b90107d296b557387c72794cad351a16
DeeonStjean <deeonstjean101@gmail.com>
2024-08-18 03:46:04 -0400

    added a nav bar

---
+++ index.js
@@ -0,0 +1,29 @@
+var menuLinks = [
+    {text: 'Home', href: '/Home'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'Anime', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+];
+
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+menuLinks.forEach((link)=>{
+    const a=document.createElement("a");
+    a.setAttribute("href",link.href);
+    a.textContent=link.text;
+    topMenuEl.appendChild(a);
+});
